bff152bb897ef31440a04469aa54c6a2493e2f41
nolevit1 <nolevit1@students.zhaw.ch>
2024-05-16 10:25:22 +0200

    Fehlende Klasse erstellt (Schritt 2)

---
+++ app/src/main/java/org/example/ScoreKeeper.java
@@ -0,0 +1,40 @@
+package org.example;
+
+public class ScoreKeeper {
+    private int scoreTeamA;
+    private int scoreTeamB;
+
+    public ScoreKeeper() {
+        scoreTeamA = 0;
+        scoreTeamB = 0;
+    }
+
+    public void scoreTeamA1() {
+        scoreTeamA += 1;
+    }
+
+    public void scoreTeamA2() {
+        scoreTeamA += 2;
+    }
+
+    public void scoreTeamA3() {
+        scoreTeamA += 3;
+    }
+
+    public void scoreTeamB1() {
+        scoreTeamB += 1;
+    }
+
+    public void scoreTeamB2() {
+        scoreTeamB += 2;
+    }
+
+    public void scoreTeamB3() {
+        scoreTeamB += 3;
+    }
+
+    public String getScore() {
+        return String.format("%03d:%03d", scoreTeamA, scoreTeamB);
+    }
+}
+
